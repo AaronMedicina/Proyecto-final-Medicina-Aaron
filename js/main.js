4be705038ee988dc788844e8b8be7f3e22a9d8fb
Aaron Medicina <aaronmedicina2005@gmail.com>
2024-09-19 13:32:27 -0300

    carga intermedia

--- js/main.js
+++ js/main.js
@@ -1,20 +1,63 @@
-let operador = prompt('Que operacion desea hacer: 1 - Sumar || 2 - Restar || 3 - Multiplicar || 4 - Dividir');
-let numero1 = Number(prompt('Ingrese el primer numero'));
-let numero2 = Number(prompt('Ingrese el segundo numero'));
-let total;
+class Productos {
+    constructor(nombre, marca, categoria) {
+        this.nombre = nombre;
+        this.marca = marca;
+        this.categoria = categoria;
 
-if (operador == 1){
-    total = numero1+numero2
-    alert('El resultado de la suma es de '+total)
-}else if(operador == 2){
-    total = numero1-numero2
-    alert('El resultado de la resta es de '+total)
-}else if(operador == 3){
-    total = numero1*numero2
-    alert('El resultado de la multiplicacion es de '+total)
-}else if(operador == 4){
-    total = numero1/numero2
-    alert('El resultado de la division es de '+total)
-}else{
-    alert('Opcion de operacion incorrecta')
+    }
 }
+
+const Almacen = [
+    new Productos("Fideos", "Marolio", "Comida"),
+    new Productos("Mayonesa", "Natura", "Aderezo"),
+    new Productos("Rollo de cocina", "Elegante", "Limpieza"),
+    new Productos("Vino", "Trumpeter", "Bebida"),
+    new Productos("Galletitas", "Don Satur", "Comida"),
+    new Productos("Chimichurri", "Alicante", "Aderezo"),
+    new Productos("Lavandina", "Libertador", "Limpieza"),
+    new Productos("Gaseosa", "Coca-Cola", "Bebida"),
+];
+
+function capitalize(text) {
+    const firstLetter = text.charAt (0);
+    const rest = text.slice(1);
+    return firstLetter.toUpperCase() + rest;
+
+}
+
+function buscarProducto() {
+    const opcion = parseInt (prompt("Buscar por: 1- Nombre, 2- Marca, 3- Categoria"));
+
+    let resultados = [];
+
+    switch(opcion) {
+        case 1:
+            const nombre = capitalize(prompt("Ingrese el nombre del producto:"));
+            resultados = Almacen.filter(Productos => Productos.nombre.includes(nombre));
+            break;
+
+        case 2:
+            const marca = capitalize(prompt("Ingrese la marca del producto:"));
+            resultados = Almacen.filter(Productos => Productos.marca.includes(marca));
+            break;
+
+        case 3:
+            const categoria = capitalize(prompt("Ingrese la categoria del producto:"));
+            resultados = Almacen.filter(Productos => Productos.categoria.includes(categoria));
+            break;
+
+        default:
+            console.log("Opción incorrecta. Vuelva a intentar.");
+    }
+
+    if (resultados.length > 0) {
+        console.log("Productos encontrados:");
+
+        resultados.forEach(Productos => {
+            console.log('Nombre: ${producto.nombre}, Marca: ${producto.marca}, Categoria: ${producto.categoria}');
+        });
+    } else {
+        console.log("No se encontraron productos.");
+    }
+}
+
