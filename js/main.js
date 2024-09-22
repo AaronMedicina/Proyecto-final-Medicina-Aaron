c1c934d9a88a42c45a7595dfb07d15d2eee0f403
Aaron Medicina <aaronmedicina2005@gmail.com>
2024-09-22 01:06:02 -0300

    carga intermedia

--- js/main.js
+++ js/main.js
@@ -88,3 +88,54 @@ function modificarProducto() {
     }
 }
 
+function eliminarProducto() {
+    const nombre = capitalize(prompt("Ingrese el nombre del producto que quiere eliminar:"));
+    const indice = Almacen.findIndex(producto => producto.nombre === nombre);
+
+    if (indice !== -1) {
+        Almacen.splice(indice, 1);
+        console.log("Producto eliminado de manera exitosa");
+    } else {
+        console.log("Producto no encontrado");
+    }
+}
+
+function mostrarInventario() {
+    Almacen.forEach((producto, index) => {
+        console.log('${index + 1}. Nombre: ${producto.nombre}, Marca: ${producto.marca}, Categoria: ${producto.categoria}');
+    });
+}
+
+do {
+    opciones = parseInt(prompt("1- Buscar un producto 2- Agregar un producto, 3- Modificar un producto, 4- Eliminar un producto, 5- Mostrar biblioteca, o 0- Salir"));
+
+    switch(opciones) {
+        case 1:
+            buscarProducto();
+        break;
+
+        case 2:
+            agregarProducto();
+        break;
+
+        case 3:
+            modificarProducto();
+        break;
+
+        case 4:
+            eliminarProducto();
+        break;
+
+        case 5:
+            mostrarInventario();
+        break;
+
+        case 0:
+            console.log("Saliendo del sistema....");
+            console.log("Presiona F5 para volver a ingresar a la biblioteca");
+        break;
+
+        default
+        console.log("Opción no válida. Ingrese un número de opción que sea válido");
+    }
+} while (opciones !== 0);
